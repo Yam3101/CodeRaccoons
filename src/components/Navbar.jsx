4da000a6713800cd8d7dfd6ec32ca635946cc8f1
Yam3101 <angelmay_810@hotmail.com>
2024-05-20 11:09:03 -0500

    changes for responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,37 @@
-import "./components.css"
+import { useState } from 'react';
+import "./components.css";
 
 function Navbar() {
+  const [isVisible, setIsVisible] = useState(false); 
+  const handleClick = () => {
+    setIsVisible(!isVisible); 
+  };
+
   return (
     <header className="bg-slate-900 text-white">
-    <nav className="flex justify-between p-6">
+      <nav className="flex justify-between p-6">
         <img src="" alt="logoCR" />
-        <ul className="flex gap-4">
-            <li className="nav-element">INICIO</li>
-            <li className="nav-element">LEARN CODE</li>
-            <li className="nav-element">ABOUT US</li>
+        <ul className="flex gap-4" id="nav-elements-cont">
+          <li className="nav-element hover:text-blue-400 cursor-pointer">INICIO</li>
+          <li className="nav-element hover:text-blue-400 cursor-pointer">LEARN CODE</li>
+          <li className="nav-element hover:text-blue-400 cursor-pointer">ABOUT US</li>
         </ul>
-    </nav>
+        <button className='hidden' id="nav-button" onClick={handleClick}>
+          <img width={30} src="/navButton.svg" alt="" />
+        </button>
+      </nav>
+      <div
+        id="hidden-elements"
+        style={{ display: isVisible ? 'flex' : 'none' }}
+      >
+        <ul className="grid gap-4">
+          <li className="nav-element cursor-pointer p-4 hover:text-blue-400">INICIO</li>
+          <li className="nav-element cursor-pointer p-4 hover:text-blue-400">LEARN CODE</li>
+          <li className="nav-element cursor-pointer p-4 hover:text-blue-400">ABOUT US</li>
+        </ul>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
